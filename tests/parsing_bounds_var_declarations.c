e070e5c4c50d386f6a9d8ab0b107f38c3a399eae
David Tarditi <dtarditi@microsoft.com>
2016-07-14 13:11:54 -0700

    Add feature tests for parsing bounds declarations.
    This change adds:
    - Feature tests for parsing variable declarations with bounds
      declarations.  Most of the tests are for local variable declarations,
      with the assumption that parsing of bounds declarations themselves will
      be similar for variables with different linkage and storage classes.
      The tests also include tests for global variables with external and
      internal linkage.
    - Feature tests for parsing function pointers where there are bounds on
      function parameters.
    
    It also renames test files so that tests will be easier to find.  The
    general format is:
      {functionality}-{area}-{sub area}.c, where {functionality} is one
      of parsing and typechecking. Area describes what is being tested, such
      as types, bounds, or something concrete.  Sub-area is optional and divides
      the area.
    With the prior naming scheme, functionality is at the end of the name,
    not the beginning, making it harder to find tests.

---
+++ tests/parsing_bounds_var_declarations.c
@@ -0,0 +1,141 @@
+// Feature tests of parsing new Checked C bounds declarations.
+//
+// The following lines are for the LLVM test harness:
+//
+// RUN: %clang_cc1 -verify -fcheckedc-extension %s
+
+
+// Top level declarations.
+extern array_ptr<int> a : count(5);
+extern array_ptr<int> b : count(3 + 2);
+extern int cLen;
+extern array_ptr<int> c : count(cLen);
+extern array_ptr<int> d : byte_count(20);
+extern array_ptr<int> e : byte_count(5 * sizeof(int));
+extern array_ptr<int> f : byte_count(cLen * sizeof(int));
+extern array_ptr<int> g : bounds(none);
+extern array_ptr<int> h : bounds(f - 2, f + 3);
+extern array_ptr<int> i : count(5), j : bounds(i - 2, i + 3),
+                      k : bounds(j + 2, j + 5);
+
+array_ptr<int> b : count(3 + 2) = 0;
+array_ptr<int> d : byte_count(5 * sizeof(int)) = 0;
+array_ptr<int> g : bounds(none) = 0;
+
+static float vals[5] = { 0.0, 1.0, 2.0, 3.0, 4.0 };
+static array_ptr<float> l : count(5) = vals;
+
+// Declare a bounds for a checked array.  This is legal, but
+// unnecessary.
+char buf checked[128] : count(128);
+array_ptr<char> cursor : bounds(buf, buf + 128) = buf + 64;
+
+// Local variable declarations.
+extern void f1(array_ptr<int> arr : count(5)) {
+  array_ptr<int> p : count(5) = arr;
+  array_ptr<int> q : count(3 + 2) = arr;
+  int len = 5;
+  array_ptr<int> r : count(len) = arr;
+  array_ptr<int> s : byte_count(20) = arr;
+  array_ptr<int> t : byte_count(5 * sizeof(int)) = arr;
+  array_ptr<int> u : byte_count(len * sizeof(int)) = arr;
+  array_ptr<int> v : bounds(v, v + 5) = arr;
+  array_ptr<int> w : bounds(none) = arr;
+  array_ptr<int> x : bounds(none);
+  array_ptr<int> midarr : bounds(midarr - 1, midarr - 1 + 2) = arr + 2;
+}
+
+// Multiple declarators in one declaration
+extern void f2(array_ptr<int> arr : count(5)) {
+  array_ptr<int> p : count(5) = 0, q : count(3 + 2) = arr;
+  int len = 5;
+  array_ptr<int> r : count(len) = arr, s : byte_count(20) = arr;
+  array_ptr<int> t : byte_count(5 * sizeof(int)) = arr,
+                 u : bounds(u, u + 5) = arr,
+                 v : bounds(none) = arr;
+}
+
+// Parenthesized subexpressions
+extern void f3(array_ptr<int> arr : count(5)) {
+  array_ptr<int> p : count((5)) = arr;
+  array_ptr<int> q : count(3 + (2)) = arr;
+  int len = 5;
+  array_ptr<int> r : count((len)) = arr;
+  array_ptr<int> s : byte_count((20)) = arr;
+  array_ptr<int> t : byte_count(5 * (sizeof(int))) = arr;
+  array_ptr<int> u : byte_count((len) * sizeof(int)) = arr;
+  array_ptr<int> v : bounds(v, (v + 5) + len - len) = arr;
+  array_ptr<int> w: bounds((w + len - (len)), (w + len));
+  array_ptr<int> midarr : bounds(midarr - 1, (midarr - 1) + 2) = arr + 2;
+}
+
+
+// Contextual keywords
+extern void f4(array_ptr<int> arr : count(len), int len) {
+  int count = len;
+  array_ptr<int> p : count(count) = arr;
+  int none = 0;
+  // none is a contextual a keyword if it follows 'bounds' '('
+  array_ptr<int> q : bounds(none) = arr;
+  array_ptr<int> r : bounds(none + arr, arr + len) = arr; // expected-error {{expected ')'}} expected-note {{to match this '('}}
+  array_ptr<int> s : bounds(arr + none, arr + len) = arr;
+  array_ptr<int> t : bounds(t, t + count) = arr;
+  int bounds = len;
+  array_ptr<int> u : bounds(u, u + bounds) = arr;
+}
+
+// Checked array declarations
+extern void f5() {
+  int arr1 checked[5] : count(5);
+  int arr2 checked[5]: count(2 + 3);
+  int arr3 checked[6] : count(5);
+  int len = 5;
+  int arr4 checked[5] : count(len);
+  int arr7 checked[5] : byte_count(5 * sizeof(int));
+  int arr8 checked[5] : byte_count(len * sizeof(int));
+  int arr5 checked[5] : bounds(arr5, arr5 + 5);
+  int arr6 checked[5] : bounds(arr5, arr5 + len);
+}
+
+// Parsing of more complicated declarations with bounds declarations
+// 
+extern void f6(int *arr checked[] : count(5)) {
+  array_ptr<int *> p : count(5) = arr;
+  array_ptr<int *> q : count(3 + 2) = arr;
+  int len = 5;
+  array_ptr<int *> r : count(len) = arr;
+  array_ptr<int *> s : byte_count(5 * sizeof(int)) = arr;
+  array_ptr<int *> t : byte_count(len * sizeof(int)) = arr;
+  // redundant but legal bounds expressions on arrays.
+  int *arr1 checked[5] : count(5);
+  int *arr2 checked[5] : count(3 + 2);
+  int *arr3 checked[5] : count(len);
+  // Checked array of unchecked pointers to functions.  We have to parenthesize
+  // the inner declarator to avoid : count(len) being parsed as part of the 
+  // type of arr, not a bounds expression.
+  int ((*arr4 checked[5])(int, int)) : count(5);
+  int ((*arr5 checked[5])(int, int)) : count(len);
+  int ((*arr6 checked[5])(int, int)) : bounds(arr5, arr5 + len);
+  // Checked array of checked pointers to functions
+  ptr<int (int, int)> arr7 checked[5] : count(5);
+  ptr<int (int, int)> arr8 checked[5] : bounds(arr8, arr8 + 5);
+  // Array_ptrs to checked pointers to functions.
+  array_ptr<ptr<int (int, int)>> u : count(5) = 0;
+  array_ptr<ptr<int (int, int)>> v : bounds(v, v + 5) = 0;
+  array_ptr<ptr<int (int, int)>> w : bounds(v, v + 5) = 0;
+  array_ptr<ptr<int (int len, array_ptr<int> arr : count(len))>> x : bounds(x, x + 5) = 0;
+}
+
+// Bounds expressions with incorrect syntax or semantics
+extern void f7(array_ptr<int> arr : count(5)) {
+  array_ptr<int> p : bounds(start, end + 5) = 0; // expected-error {{undeclared identifier 'start'}} expected-error {{undeclared identifier 'end'}}
+  array_ptr<int> q : count(len) = 0;       // expected-error {{undeclared identifier 'len'}}
+  array_ptr<int> r : byte_count(len) = 0;  // expected-error {{undeclared identifier 'len'}}
+  array_ptr<int> s : 6 + 6 = 0;            // expected-error {{expected bounds expression}}
+  array_ptr<int> t : 6 + 6, u : count(5) = arr; // expected-error {{expected bounds expression}}
+  array_ptr<int> v : boounds(a, b + 5) = 0;  // expected-error {{expected bounds expression}}
+  array_ptr<int> w : coount(5) = 0;         // expected-error {{expected bounds expression}}
+  array_ptr<ptr<int (int len, array_ptr<int> arr : count(badvar))>> x // expected-error {{undeclared identifier 'badvar'}}
+    : bounds(x, x + 5) = 0;
+}
+
