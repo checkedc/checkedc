93136a83996f105d9ad08a60c7edb0a0f3cdabc5
David Tarditi <dtarditi@microsoft.com>
2016-07-15 12:48:50 -0700

    Add feature tests for parsing bounds declarations. (#33)
    This change adds:
    - Feature tests for parsing variable declarations with bounds
      declarations.  Most of the tests are for local variable declarations,
      with the assumption that parsing of bounds declarations themselves will
      be similar for variables with different linkage and storage classes.
      The tests also include tests for the other storage classes and linkage
      combinations that C allows.
    - Feature tests for parsing function pointers where there are bounds on
      function parameters.
    
    It also moves files to subdirectories so that tests will be easier to find.
    The subdirectories are named after the functionality that is being tested
    (parsing and typechecking, so far).   With the prior naming scheme, 
    functionality was included at the end of the file name, making it harder to find 
    tests.

--- tests/parsing/parameter_bounds.c
+++ tests/parsing/parameter_bounds.c
@@ -36,7 +36,7 @@ extern void f7a(array_ptr<int> midarr :
 extern void f8(array_ptr<int> arr : bounds(none)) {
 }
 
-// count is a contextual keyword.  It is only a keyword when it
+// 'count' is a contextual keyword.  It is only a keyword when it
 // immediately follows the ':' in a bounds declaration.
 extern void f9(int count, array_ptr<int> arr : count(count)) {
 }
@@ -44,7 +44,7 @@ extern void f9(int count, array_ptr<int> arr : count(count)) {
 extern void f9a(array_ptr<int> arr : count(count), int count) {
 }
 
-// none is a contextual keyword.  It is only a keyword when it
+// 'none' is a contextual keyword.  It is only a keyword when it
 // immediately follows the ':' in a bounds declaration.
 extern void f10(int none, array_ptr<int> arr : count(none)) {
 }
@@ -52,7 +52,7 @@ extern void f10(int none, array_ptr<int> arr : count(none)) {
 extern void f10a(array_ptr<int> arr : count(none), int none) {
 }
 
-// bounds is a contextual keyword.  It is only a keyword when it
+// 'bounds' is a contextual keyword.  It is only a keyword when it
 // immediately follows the ':' in a bounds declaration.
 extern void f11(int bounds, array_ptr<int> arr : bounds(arr, arr + 5)) {
 }
@@ -96,7 +96,7 @@ extern void f18(int len, int arr checked[] : count(len)) {
 extern void f18a(int arr checked[] : count(len), int len) {
 }
 
-// declare arrays of pointers to integers with bounds declarations
+// Declare arrays of pointers to integers with bounds declarations
 extern void f19(int *arr checked[] : count(5)) {
 }
 
@@ -129,12 +129,12 @@ extern void f23a(int((*arr checked[])(int, int)) : bounds(arr, arr + len), int l
 extern void f24(int arr : count(len)) { // expected-error {{use of undeclared identifier 'len'}}
 }
 
-// check parsing of a function with lots of arguments.
+// Check parsing of a function with lots of arguments.
 extern void f25(int a, int b, int c, int d, float f, int g, int h, int i,
                 int j, int k, int len, int arr : count(len)) {
 }
 
-// intentionally misspell bounds to cause a parsing error.
+// Misspell bounds to cause a parsing error.
 extern void f26(int len,
                 int arr : boounds(arr, arr + len)) { // expected-error {{expected bounds expression}}
 }
@@ -144,7 +144,7 @@ extern void f27(int len,
                 int flag) {
 }
 
-// intentionally misspell count to cause a parsing error.
+// Misspell count to cause a parsing error.
 extern void f28(int len,
                 int arr : coount(len)) { // expected-error {{expected bounds expression}}
 }
@@ -154,7 +154,7 @@ extern void f29(int len,
                 int flag) {
 }
 
-//  bounds expressions with parenthesized subexpressions
+//  Bounds expressions with parenthesized subexpressions.
 extern void f30(int len,
                 int arr : bounds(arr, (arr + len))) {
 }
@@ -168,3 +168,39 @@ extern void f32(int len,
                 int arr : count((len)),
                 int flag) {
 }
+
+// Pointers to functions that have bounds declarations on parameters.
+extern void f33() {
+  // Checked pointer to a function that takes an array_ptr to 5 integers.
+  ptr<int(array_ptr<int> : count(5))> p1 = 0;
+  ptr<int(array_ptr<int> arg : count(5))> p2 = 0;
+  // Checked pointer to a function that takes an array_ptr to n integers,
+  // where n is another argument.
+  ptr<int (array_ptr<int> arg : count(n), int n)> p3  = 0;
+  ptr<int (int n, array_ptr<int> arg : count(n))> p4  = 0;
+  // Use 'byte_count; instead of 'count'
+  ptr<int (array_ptr<int> : byte_count(5 * sizeof(int)))> q1 = 0;
+  ptr<int (array_ptr<int> arg : byte_count(5 * sizeof(int)))> q2 = 0;
+  ptr<int (array_ptr<int> arg : byte_count(n * sizeof(int)), int n)> q3 = 0;
+  ptr<int(int n, array_ptr<int> arg : byte_count(n * sizeof(int)))> q4 = 0;
+  // Use 'bounds' instead of 'count'
+  ptr<int (array_ptr<int> arg : bounds(arg, arg + 5))> r1 = 0;
+  ptr<int (array_ptr<int> arg : bounds(arg, arg + n), int n)> r2 = 0;
+  ptr<int (int n, array_ptr<int> arg : bounds(arg, arg + n))> r3 = 0;
+  // Unchecked pointers to functions.
+ int (*s1)(array_ptr<int> : count(5)) = 0;
+ int (*s2)(array_ptr<int> arg : count(5)) = 0;
+ int (*s3)(int n, array_ptr<int> arg : count(n)) = 0;
+ int (*s4)(array_ptr<int> arg : count(n), int n) = 0;
+ // Use 'byte_count' instead of 'count'
+ int (*t1)(array_ptr<int> : byte_count(5 * sizeof(int))) = 0;
+ int (*t2)(array_ptr<int> arg : count(5 * sizeof(int))) = 0;
+ int (*t3)(int n, array_ptr<int> arg : count(n * sizeof(int))) = 0;
+ int (*t4)(array_ptr<int> arg : count(n * sizeof(int)), int n) = 0;
+}
+
+typedef int func1(array_ptr<int> : count(len), int len);
+typedef int func2(int len, array_ptr<int> : count(len));
+static func1 *func1_ptr1;
+static func2 *func2_ptr1;
+func1 *func1_ptr2;
