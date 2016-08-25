c6c7a38a85b7dd70e87ec51df8307fda950ece20
David Tarditi <dtarditi@microsoft.com>
2016-08-25 15:38:59 -0700

    Update based on code review feedback.
    - Add negative tests cases for functions, function pointers,
    floating point literals.
    - Add a few spot checks that type checking of bounds expressions using
    file scope or globally scoped variables works as expected.
    - Test a few cases involving the use of integer literals.

--- tests/typechecking/bounds.c
+++ tests/typechecking/bounds.c
@@ -6,7 +6,11 @@
 
 // Test expressions with standard signed and unsigned integers types as
 // arguments to count and byte_count.
-extern void f1() {
+
+static int A = 8;
+static long long int B = 8;
+
+extern void f1(void) {
 
   char c1 = 8;
   short c2 = 8;
@@ -32,23 +36,39 @@ extern void f1() {
   array_ptr<int> t9 : count(c9) = 0;
   array_ptr<int> t10 : count(c10) = 0;
   array_ptr<int> t11 : count(c11) = 0;
+  // Spot-check type checking of count expressions involving file-scoped
+  // variables.
+  array_ptr<int> t12 : count(A) = 0;
+  array_ptr<int> t13 : count(B) = 0;
+  array_ptr<int> t14 : count(12) = 0;
+  // This will eventually fail static checking of bounds declarations, at which
+  // point we'll need to add an expected error message.
+  array_ptr<int> t15 : count(-5) =  0;
 
-  array_ptr<int> t12 : byte_count(c1) = 0;
-  array_ptr<int> t13 : byte_count(c2) = 0;
-  array_ptr<int> t14 : byte_count(c3) = 0;
-  array_ptr<int> t15 : byte_count(c4) = 0;
-  array_ptr<int> t16 : byte_count(c5) = 0;
-  array_ptr<int> t17 : byte_count(c6) = 0;
-  array_ptr<int> t18 : byte_count(c7) = 0;
-  array_ptr<int> t19 : byte_count(c8) = 0;
-  array_ptr<int> t20 : byte_count(c9) = 0;
-  array_ptr<int> t21 : byte_count(c10) = 0;
-  array_ptr<int> t22 : byte_count(c11) = 0;
+  array_ptr<int> t16 : byte_count(c1) = 0;
+  array_ptr<int> t17 : byte_count(c2) = 0;
+  array_ptr<int> t18 : byte_count(c3) = 0;
+  array_ptr<int> t19 : byte_count(c4) = 0;
+  array_ptr<int> t20 : byte_count(c5) = 0;
+  array_ptr<int> t21 : byte_count(c6) = 0;
+  array_ptr<int> t22 : byte_count(c7) = 0;
+  array_ptr<int> t23 : byte_count(c8) = 0;
+  array_ptr<int> t24 : byte_count(c9) = 0;
+  array_ptr<int> t25 : byte_count(c10) = 0;
+  array_ptr<int> t26 : byte_count(c11) = 0;
+  // Spot-check type checking of byte_count expressions involving file-scoped
+  // variables.
+  array_ptr<int> t27 : byte_count(A) = 0;
+  array_ptr<int> t28 : byte_count(B) = 0;
+  array_ptr<int> t29 : byte_count(12) = 0;
+  // This will eventually fail static checking of bounds declarations, at which
+  // point we'll need to add an expected error message.
+  array_ptr<int> t30 : byte_count(-8) = 0;
 }
 
 // Test expressions involving enum names and member bit fields that can be
 // converted to integer types as arguments to count and byte_count.
-extern void f2() {
+extern void f2(void) {
   enum E {
     c1 = 8,
     c2
@@ -64,8 +84,10 @@ extern void f2() {
   array_ptr<int> t3 : count(s.f) = 0;
 }
 
+float globalFloat = 8;
+
 // Test types that should not work as arguments for count and byte_count.
-extern void f3() {
+extern void f3(void) {
 
   float c1 = 8.0;
   double c2 = 8.0;
@@ -86,6 +108,8 @@ extern void f3() {
     float f;
   } s = { 8.0 };
 
+  void (*func_ptr)(void) = f2;
+
 #ifndef __STDC_NO_COMPLEX__
  float _Complex c8 = 8.0;
 #endif
@@ -98,20 +122,31 @@ extern void f3() {
   array_ptr<int> t6 : count(c6) = 0;   // expected-error {{invalid argument type}}
   array_ptr<int> t7 : count(c7) = 0;   // expected-error {{invalid argument type}}
   array_ptr<int> t8 : count(s.f) = 0;  // expected-error {{invalid argument type}}
+  array_ptr<int> t9 : count(f2) = 0; // expected-error {{invalid argument type}}
+  array_ptr<int> t10 : count(func_ptr) = 0; // expected-error {{invalid argument type}}
+  array_ptr<int> t11 : count("test") = 0; // expected-error {{invalid argument type}}
+  array_ptr<int> t12 : count(5.0f) = 0; // expected-error {{invalid argument type}}
+  array_ptr<int> t13 : count(globalFloat) = 0; // expected-error {{invalid argument type}}
+
 #ifndef __STDC_NO_COMPLEX__
-  array_ptr<int> t9 : count(c8) = 0;   // expected-error {{invalid argument type}}
+  array_ptr<int> t14 : count(c8) = 0;   // expected-error {{invalid argument type}}
 #endif
 
-  array_ptr<int> t10 : byte_count(c1) = 0;   // expected-error {{invalid argument type 'float' to byte_count expression}}
-  array_ptr<int> t11 : byte_count(c2) = 0;  // expected-error {{invalid argument type}}
-  array_ptr<int> t12 : byte_count(c3) = 0;  // expected-error {{invalid argument type}}
-  array_ptr<int> t13 : byte_count(c4) = 0;  // expected-error {{invalid argument type}}
-  array_ptr<int> t14 : byte_count(c5) = 0;  // expected-error {{invalid argument type}}
-  array_ptr<int> t15 : byte_count(c6) = 0;  // expected-error {{invalid argument type}}
-  array_ptr<int> t16 : byte_count(c7) = 0;  // expected-error {{invalid argument type}}
-  array_ptr<int> t17 : byte_count(s.f) = 0;  // expected-error {{invalid argument type}}
-#ifndef __STDC_NO_COMPLEX__
-  array_ptr<int> t18 : byte_count(c8) = 0;   // expected-error {{invalid argument type}}
-#endif
+  array_ptr<int> t15 : byte_count(c1) = 0;  // expected-error {{invalid argument type 'float' to byte_count expression}}
+  array_ptr<int> t16 : byte_count(c2) = 0;  // expected-error {{invalid argument type}}
+  array_ptr<int> t17 : byte_count(c3) = 0;  // expected-error {{invalid argument type}}
+  array_ptr<int> t18 : byte_count(c4) = 0;  // expected-error {{invalid argument type}}
+  array_ptr<int> t19 : byte_count(c5) = 0;  // expected-error {{invalid argument type}}
+  array_ptr<int> t20 : byte_count(c6) = 0;  // expected-error {{invalid argument type}}
+  array_ptr<int> t21 : byte_count(c7) = 0;  // expected-error {{invalid argument type}}
+  array_ptr<int> t22 : byte_count(s.f) = 0; // expected-error {{invalid argument type}}
+  array_ptr<int> t23 : byte_count(func_ptr) = 0;  // expected-error {{invalid argument type}}
+  array_ptr<int> t24 : byte_count("test") = 0; // expected-error {{invalid argument type}}
+  array_ptr<int> t25 : byte_count(5.0f) = 0; // expected-error {{invalid argument type}}
+  array_ptr<int> t26 : byte_count(globalFloat) = 0; // expected-error {{invalid argument type}}
 
+#ifndef __STDC_NO_COMPLEX__
+  array_ptr<int> t27 : byte_count(c8) = 0;   // expected-error {{invalid argument type}}
+#endif
 }
+
