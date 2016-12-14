16c6bbabff4bf516a4127313e451ac87cd94e6b7
David Tarditi <dtarditi@microsoft.com>
2016-12-14 15:25:14 -0800

    Update error messages for bounds mismatches.
    The clang implementation now prints more specific error messages for
    bounds mismatches for function declarations.  Updates the error messages
    in the tests for bounds mismatches.

--- tests/typechecking/redeclarations.c
+++ tests/typechecking/redeclarations.c
@@ -68,28 +68,28 @@ int *f25(int len);
 
 void f30(int *p : count(len), int len);
 void f30(int *p : count(len), int len);
-void f30(int *p : count(len + 1), int len);  // expected-error {{conflicting types for 'f30'}}
+void f30(int *p : count(len + 1), int len);  // expected-error {{conflicting parameter bounds}}
 
 void f31(int *p : count(len), int len);
 // A redeclaration without bounds-safe interface is compatible with the
 // original declaration, but the function retains its original bounds-safe
 // interface.
 void f31(int *p, int len);
-void f31(int *p : count(len + 1), int len);  // expected-error {{conflicting types for 'f31'}}
+void f31(int *p : count(len + 1), int len);  // expected-error {{conflicting parameter bounds}}
 
 void f32(int *p : bounds(p, p + len), int len);
 void f32(int *p : bounds(p, p + len), int len);
-void f32(int *p : bounds(p, p + len + 1), int len);  // expected-error {{conflicting types for 'f32'}}
+void f32(int *p : bounds(p, p + len + 1), int len);  // expected-error {{conflicting parameter bounds}}
 
 void f33(int *p : bounds(p, p + len), int len);
 void f33(int *p, int len);
 // A redeclaration without a bounds-safe interface is compatible with the
 // original declaration, but the function retains its original bounds-safe
 // interface
-void f33(int *p : bounds(p, p + len + 1), int len);  // expected-error {{conflicting types for 'f33'}}
+void f33(int *p : bounds(p, p + len + 1), int len);  // expected-error {{conflicting parameter bounds}}
 
 void f34(int *p : itype(ptr<int>), int len);
-void f34(int *p : count(len), int len);  // expected-error {{conflicting types for 'f34'}}
+void f34(int *p : count(len), int len);  // expected-error {{conflicting parameter bounds}}
 
 //---------------------------------------------------------------------------//
 // Redeclarations of functions that have parameters that have bounds         //
@@ -97,11 +97,11 @@ void f34(int *p : count(len), int len);  // expected-error {{conflicting types f
 //---------------------------------------------------------------------------//
 void f40(array_ptr<int> p : count(len), int len);
 void f40(array_ptr<int> p : count(len), int len);
-void f40(array_ptr<int> p : count(len + 1), int len); // expected-error {{conflicting types for 'f40'}}
+void f40(array_ptr<int> p : count(len + 1), int len); // expected-error {{conflicting parameter bounds}}
 
 void f41(array_ptr<int> p : bounds(p, p + len), int len);
 void f41(array_ptr<int> p : bounds(p, p + len), int len);
-void f41(array_ptr<int> p : bounds(p, p + len + 1), int len); // expected-error {{conflicting types for 'f41'}}
+void f41(array_ptr<int> p : bounds(p, p + len + 1), int len); // expected-error {{conflicting parameter bounds}}
 
 
 //---------------------------------------------------------------------------//
@@ -110,28 +110,28 @@ void f41(array_ptr<int> p : bounds(p, p + len + 1), int len); // expected-error 
 //---------------------------------------------------------------------------//
 int *f50(int len) : count(len);
 int *f50(int len) : count(len);
-int *f50(int len) : count(len + 1);  // expected-error {{conflicting types for 'f50'}}
+int *f50(int len) : count(len + 1);  // expected-error {{conflicting return bounds}}
 
 int *f51(int len) : count(len);
 // A redeclaration without bounds-safe interface is compatible with the
 // original declaration, but the function retains its original bounds-safe
 // interface.
 int *f51(int len);
-int *f51(int len) : count(len + 1);  // expected-error {{conflicting types for 'f51'}}
+int *f51(int len) : count(len + 1);  // expected-error {{conflicting return bounds}}
 
 int *f52(int *p : bounds(p, p + len), int len) : bounds(p, p + len);
 int *f52(int *p : bounds(p, p + len), int len) : bounds(p, p + len);
-int *f52(int *p : bounds(p, p + len), int len) : bounds(p, p + len + 1);  // expected-error {{conflicting types for 'f52'}}
+int *f52(int *p : bounds(p, p + len), int len) : bounds(p, p + len + 1);  // expected-error {{conflicting return bounds}}
 
 int *f53(int *p : bounds(p, p + len), int len) : bounds(p, p + len);
 int *f53(int *p, int len);
 // A redeclaration without a bounds-safe interface is compatible with the
 // original declaration, but the function retains its original bounds-safe
 // interface.
-int *f53(int *p : bounds(p, p + len), int len) : bounds(p, p + len + 1);  // expected-error {{conflicting types for 'f53'}}
+int *f53(int *p : bounds(p, p + len), int len) : bounds(p, p + len + 1);  // expected-error {{conflicting return bounds}}
 
 int *f54(int len) : itype(ptr<int>);
-int *f54(int len) : count(len);  // expected-error {{conflicting types for 'f54'}}
+int *f54(int len) : count(len);  // expected-error {{conflicting return bounds}}
 
 //---------------------------------------------------------------------------//
 // Redeclarations of functions that have bounds declarations for returns     //
@@ -139,14 +139,14 @@ int *f54(int len) : count(len);  // expected-error {{conflicting types for 'f54'
 //---------------------------------------------------------------------------//
 array_ptr<int> f60(int len) : count(len);
 array_ptr<int> f60(int len) : count(len);
-array_ptr<int> f60(int len) : count(len + 1); // expected-error {{conflicting types for 'f60'}}
+array_ptr<int> f60(int len) : count(len + 1); // expected-error {{conflicting return bounds}}
 
 array_ptr<int> f61(array_ptr<int> p : count(len), int len) : bounds(p, p + len);
 array_ptr<int> f61(array_ptr<int> p : count(len), int len) : bounds(p, p + len);
-array_ptr<int> f61(array_ptr<int> p : count(len), int len) : bounds(p, p + len - 1); // expected-error {{conflicting types for 'f61'}}
+array_ptr<int> f61(array_ptr<int> p : count(len), int len) : bounds(p, p + len - 1); // expected-error {{conflicting return bounds}}
 
 //---------------------------------------------------------------------------//
-// Redeclarations of functions that have paraemters with function pointer    //
+// Redeclarations of functions that have parameters with function pointer    //
 // types that have bounds-safe interfaces must have matching bounds-safe     //
 // interfaces.                                                               //
 //---------------------------------------------------------------------------//
@@ -159,12 +159,12 @@ void f70(int * (fn(int *, int *)) :
 void f70(int * (fn(int *, int *)) :
            itype(array_ptr<int> (ptr<int>, ptr<int>)));
 // return type of itype differs.
-void f70(int (*fn(int *, int *)) : itype(ptr<int> (ptr<int>, ptr<int>))); // expected-error {{conflicting types for 'f70'}}
+void f70(int (*fn(int *, int *)) : itype(ptr<int> (ptr<int>, ptr<int>))); // expected-error {{conflicting parameter bounds}}
 
 // Interface type on parameters of a function pointer type
 void f71(int * fn(int *, int *));
 void f71(int * fn(int * : count(5), int *: count(5)));
-void f71(int * fn(int * : count(6), int * : count(6))); // expected-error {{conflicting types for 'f71'}}
+void f71(int * fn(int * : count(6), int * : count(6))); // expected-error {{conflicting bounds for 'f71'}}
 
 // Interface type on return value of a function pointer type
 void f72(int * fn(int *, int *));
@@ -174,8 +174,8 @@ void f72(int * fn(int *, int *) : itype(ptr<int>));
 //---------------------------------------------------------------------------//
 
 void f80(int *p : count(len), int len);
-void f80(int *p : count((len)), int len); // expected-error {{conflicting types for 'f80'}}
+void f80(int *p : count((len)), int len); // expected-error {{conflicting parameter bounds}}
 
 void f81(int *p : count(len), int len);
-void f81(int *p : bounds(p, p + len), int len);  // expected-error {{conflicting types for 'f81'}}
+void f81(int *p : bounds(p, p + len), int len);  // expected-error {{conflicting parameter bounds}}
 
