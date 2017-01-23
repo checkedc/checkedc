3f75c524f19374b81e43918d1fa888d0bf9eda70
Archibald Samuel Elliott <t-arelli@microsoft.com>
2017-01-23 13:12:40 -0800

    Adds dynamic_check tests including arithmetic

---
+++ tests/dynamic_checking/failing-arith-dynamic_check.c
@@ -0,0 +1,19 @@
+// Feature tests of dynamic checking in Checked C
+//
+// The following lines are for the LLVM test harness:
+//
+// RUN: %clang -fcheckedc-extension -o %t.exe %s
+// LLVM thinks that exiting via llvm.trap() is a crash.
+// RUN: not --crash %t.exe
+
+#include "../../include/stdchecked.h"
+
+void f1(int i) {
+  dynamic_check(i < 30);
+}
+
+int main(void) {
+  f1(50);
+
+  return 0;
+}
