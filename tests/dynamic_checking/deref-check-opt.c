e7420b4c9c416fd3d7ecacfb2e9815dc22bcfacc
David Tarditi <dtarditi@microsoft.com>
2017-03-06 17:15:49 -0800

    Use signals for out-of-bound memory access tests. (#123)
    This way processes exit gracefully, instead of relying on the OS to handle
    a crash.  On Windows, crashes cause Windows Error Reporting (WER) to fire up,
    invoking system logging.
    
    We have been using the not program from the LLVM test harness with the --crash
    option to determine when processes have exited with a crash.  We have found
    that this approach is not working reliably on Windows.  The not program spawns
    a child process and looks at the process exit code, looking for negative exit
    codes to indicate a crash. When running automated testing under Visual Studio
    Team Services, sometimes the exit code is a positive number.
    
    Testing:
    - Changed tests run successfully for x86 Windows x64 Linux.

--- tests/dynamic_checking/deref-check-opt.c
+++ tests/dynamic_checking/deref-check-opt.c
@@ -11,10 +11,10 @@
 // RUN: %clang -fcheckedc-extension %S/deref-check.c -o %t -Werror -O3
 // RUN: %t pass1 | FileCheck %S/deref-check.c --check-prefixes=CHECK,CHECK-PASS,CHECK-PASS-1
 // RUN: %t pass2 | FileCheck %S/deref-check.c --check-prefixes=CHECK,CHECK-PASS,CHECK-PASS-2
-// RUN: not --crash %t fail1 | FileCheck %S/deref-check.c --check-prefixes=CHECK,CHECK-FAIL,CHECK-FAIL-1
-// RUN: not --crash %t fail2 | FileCheck %S/deref-check.c --check-prefixes=CHECK,CHECK-FAIL,CHECK-FAIL-2
-// RUN: not --crash %t fail3 | FileCheck %S/deref-check.c --check-prefixes=CHECK,CHECK-FAIL,CHECK-FAIL-3
-// RUN: not --crash %t fail4 | FileCheck %S/deref-check.c --check-prefixes=CHECK,CHECK-FAIL,CHECK-FAIL-4
+// RUN: %t fail1 | FileCheck %S/deref-check.c --check-prefixes=CHECK,CHECK-FAIL,CHECK-FAIL-1
+// RUN: %t fail2 | FileCheck %S/deref-check.c --check-prefixes=CHECK,CHECK-FAIL,CHECK-FAIL-2
+// RUN: %t fail3 | FileCheck %S/deref-check.c --check-prefixes=CHECK,CHECK-FAIL,CHECK-FAIL-3
+// RUN: %t fail4 | FileCheck %S/deref-check.c --check-prefixes=CHECK,CHECK-FAIL,CHECK-FAIL-4
 
 #import <stdlib.h>
 
--- tests/dynamic_checking/subscript-check-opt.c
+++ tests/dynamic_checking/subscript-check-opt.c
@@ -15,23 +15,23 @@
 // RUN: %t 2 4 8 8  0 4   0 3 2  | FileCheck %S/subscript-check.c
 // RUN: %t 2 4 8 8  1 3   0 1 5  | FileCheck %S/subscript-check.c
 // RUN: %t 2 4 8 8  2 -1  2 -1 2 | FileCheck %S/subscript-check.c
-// RUN: not --crash %t 3        | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-1
-// RUN: not --crash %t -1       | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-1
-// RUN: not --crash %t 0 5      | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-1
-// RUN: not --crash %t 0 -1     | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-1
-// RUN: not --crash %t 0 0 9    | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-1
-// RUN: not --crash %t 0 0 -1   | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-1
-// RUN: not --crash %t 0 0 0 9  | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-1
-// RUN: not --crash %t 0 0 0 -1 | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-1
-// RUN: not --crash %t 0 0 0 0  3 0   | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-2
-// RUN: not --crash %t 0 0 0 0  2 3   | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-2
-// RUN: not --crash %t 0 0 0 0  0 9   | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-2
-// RUN: not --crash %t 0 0 0 0  -1 -1 | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-2
-// RUN: not --crash %t 0 0 0 0  0 0  3 0 0    | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-3
-// RUN: not --crash %t 0 0 0 0  0 0  2 9 0    | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-3
-// RUN: not --crash %t 0 0 0 0  0 0  2 2 3    | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-3
-// RUN: not --crash %t 0 0 0 0  0 0  0 0 27   | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-3
-// RUN: not --crash %t 0 0 0 0  0 0  -1 -1 -1 | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-3
+// RUN: %t 3        | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-1
+// RUN: %t -1       | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-1
+// RUN: %t 0 5      | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-1
+// RUN: %t 0 -1     | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-1
+// RUN: %t 0 0 9    | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-1
+// RUN: %t 0 0 -1   | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-1
+// RUN: %t 0 0 0 9  | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-1
+// RUN: %t 0 0 0 -1 | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-1
+// RUN: %t 0 0 0 0  3 0   | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-2
+// RUN: %t 0 0 0 0  2 3   | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-2
+// RUN: %t 0 0 0 0  0 9   | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-2
+// RUN: %t 0 0 0 0  -1 -1 | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-2
+// RUN: %t 0 0 0 0  0 0  3 0 0    | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-3
+// RUN: %t 0 0 0 0  0 0  2 9 0    | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-3
+// RUN: %t 0 0 0 0  0 0  2 2 3    | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-3
+// RUN: %t 0 0 0 0  0 0  0 0 27   | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-3
+// RUN: %t 0 0 0 0  0 0  -1 -1 -1 | FileCheck %S/subscript-check.c --check-prefix=CHECK-FAIL-3
 
 #include <stdlib.h>
 
