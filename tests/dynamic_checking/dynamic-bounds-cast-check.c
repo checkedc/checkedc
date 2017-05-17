fca418f7326602b753a86401bec37f36a13f2b7d
wonsub kim <wonsub79.kim@samsung.com>
2017-05-17 11:06:25 +0900

    + modify test codes

--- tests/dynamic_checking/dynamic-bounds-cast-check.c
+++ tests/dynamic_checking/dynamic-bounds-cast-check.c
@@ -2,9 +2,6 @@
 //
 // RUN: %clang -fcheckedc-extension %s -o %t -Werror
 // RUN: %t pass1 | FileCheck %s --check-prefixes=CHECK,CHECK-PASS,CHECK-PASS-1
-// RUN: %t pass2 | FileCheck %s --check-prefixes=CHECK,CHECK-PASS,CHECK-PASS-2
-// RUN: %t pass3 | FileCheck %s --check-prefixes=CHECK,CHECK-PASS,CHECK-PASS-3
-// RUN: %t pass4 | FileCheck %s --check-prefixes=CHECK,CHECK-PASS,CHECK-PASS-4
 // RUN: %t fail1 | FileCheck %s --check-prefixes=CHECK,CHECK-FAIL,CHECK-FAIL-1
 // RUN: %t fail2 | FileCheck %s --check-prefixes=CHECK,CHECK-FAIL,CHECK-FAIL-2
 // RUN: %t fail3 | FileCheck %s --check-prefixes=CHECK,CHECK-FAIL,CHECK-FAIL-3
@@ -18,9 +15,6 @@
 #include "../../include/stdchecked.h"
 
 void passing_test_1(void);
-void passing_test_2(void);
-void passing_test_3(void);
-void passing_test_4(void);
 
 void failing_test_1(void);
 void failing_test_2(void);
@@ -63,24 +57,12 @@ int main(int argc, array_ptr<char*> argv : count(argc)) {
   puts("Starting Test");
 
   if (strcmp(argv[1], "pass1") == 0) {
-    // CHECK-PASS-1: Printable
+    // CHECK-PASS-1: Printable0
+    // CHECK-PASS-1: Printable1
+    // CHECK-PASS-1: Printable2
+    // CHECK-PASS-1: Printable3
     // CHECK-PASS-1: Expected Success
     passing_test_1();
-  }
-  else if (strcmp(argv[1], "pass2") == 0) {
-    // CHECK-PASS-2: Printable
-    // CHECK-PASS-2: Expected Success
-    passing_test_2();
-  }
-  else if (strcmp(argv[1], "pass3") == 0) {
-    // CHECK-PASS-3: Printable
-    // CHECK-PASS-3: Expected Success
-    passing_test_3();
-  }
-  else if (strcmp(argv[1], "pass4") == 0) {
-    // CHECK-PASS-4: Printable
-    // CHECK-PASS-4: Expected Success
-    passing_test_4();
   }
   else if (strcmp(argv[1], "fail1") == 0) {
     // CHECK-FAIL-1-NOT: Unprintable
@@ -93,6 +75,9 @@ int main(int argc, array_ptr<char*> argv : count(argc)) {
     failing_test_2();
   }
   else if (strcmp(argv[1], "fail3") == 0) {
+    // CHECK-FAIL-3 : Printable0
+    // CHECK-FAIL-3 : Printable1
+    // CHECK-FAIL-3 : Printable2
     // CHECK-FAIL-3-NOT: Unprintable
     // CHECK-FAIL-3-NOT: Unexpected Success
     failing_test_3();
@@ -116,45 +101,26 @@ int main(int argc, array_ptr<char*> argv : count(argc)) {
   return EXIT_SUCCESS;
 }
 
+// dynamic_check(r != NULL) && dynamic_check(r <= r && r+1 <= r+10) - > OK
 // dynamic_check(r != NULL) && dynamic_check(r <= r && r+3 <= r+10) - > OK
+// dynamic_check((r+3) != NULL) && dynamic_check(r <= r+3 && r+6 <= r+10) - > OK
+// dynamic_check((r+3) != NULL) && dynamic_check(r <= r && r+15 <= r+10) - > OK
 void passing_test_1(void) {
   ptr<int> q = 0;
   int r checked[10] = {0,1,2,3,4,5,6,7,8,9};
-  q = _Dynamic_bounds_cast<array_ptr<int>>(r, 3);
+  array_ptr<int> s : count(5) = r;
 
-  printf("Printable\n");
-
-  puts("Expected Success");
-}
-
-// dynamic_check(r != NULL) && dynamic_check(r <= r && r+1 <= r+10) - > OK
-void passing_test_2(void) {
-  ptr<int> q = 0;
-  int r checked[10] = {0,1,2,3,4,5,6,7,8,9};
   q = _Dynamic_bounds_cast<ptr<int>>(r);
+  printf("Printable0\n");
 
-  printf("Printable\n");
+  q = _Dynamic_bounds_cast<array_ptr<int>>(r, 3);
+  printf("Printable1\n");
 
-  puts("Expected Success");
-}
-
-// dynamic_check((r+3) != NULL) && dynamic_check(r <= r && r+1 <= r+10) - > OK
-void passing_test_3(void) {
-  ptr<int> q = 0;
-  int r checked[10] = {0,1,2,3,4,5,6,7,8,9};
-  q = _Dynamic_bounds_cast<ptr<int>>(r+3);
-  printf("Printable\n");
-
-  puts("Expected Success");
-}
-
-// dynamic_check((r+3) != NULL) && dynamic_check(r <= r && r+3 <= r+10) - > OK
-void passing_test_4(void) {
-  ptr<int> q = 0;
-  int r checked[10] = {0,1,2,3,4,5,6,7,8,9};
   q = _Dynamic_bounds_cast<array_ptr<int>>(r+3, 3);
+  printf("Printable2\n");
 
-  printf("Printable\n");
+  q = _Dynamic_bounds_cast<array_ptr<int>>(r, s, s+3);
+  printf("Printable3\n");
 
   puts("Expected Success");
 }
@@ -185,7 +151,18 @@ void failing_test_2(void) {
 void failing_test_3(void) {
   ptr<int> q = 0;
   int r checked[10] = {0,1,2,3,4,5,6,7,8,9};
-  array_ptr<int> s : count(5) = 0;
+  array_ptr<int> s : count(5) = r;
+
+  q = _Dynamic_bounds_cast<ptr<int>>(r);
+  printf("Printable0\n");
+
+  q = _Dynamic_bounds_cast<array_ptr<int>>(r, 5);
+  printf("Printable1\n");
+
+  q = _Dynamic_bounds_cast<array_ptr<int>>(r, s, s+3);
+  printf("Printable2\n");
+
+  s = 0;
   q = _Dynamic_bounds_cast<array_ptr<int>>(r, s, s+3);
   
   printf("Unprintable\n");
