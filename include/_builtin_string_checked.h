896b20aaa8f05d8634e511d637fd4263cc759a2e
Sam Elliott <ashe2@cs.washington.edu>
2017-11-06 19:13:41 -0800

    Add Strings.h support for _FORTIFY_SOURCE

--- include/_builtin_string_checked.h
+++ include/_builtin_string_checked.h
@@ -12,6 +12,8 @@
 
 #include "_builtin_common.h"
 
+#if _USE_FORTIFY_LEVEL > 0
+
 #ifndef __has_builtin
 #define _undef__has_builtin
 #define __has_builtin(x) 0
@@ -38,6 +40,23 @@ void *__builtin___memset_chk(void * s : byte_count(n),
                             size_t obj_size) : bounds(s, (_Array_ptr<char>) s + n);
 #endif
 
+#if __has_builtin(__builtin___strcat_chk) || defined(__GNUC__)
+// Left Unchecked Intentionally. See comment in string_checked.h
+_Unchecked
+char *__builtin___strcat_chk(char * restrict dest,
+                             const char * restrict src :
+                               itype(restrict _Nt_array_ptr<const char>),
+                             size_t obj_size);
+#endif
+
+#if __has_builtin(__builtin___strcpy_chk) || defined(__GNUC__)
+_Unchecked
+char *__builtin___strcpy_chk(char * restrict dest,
+                             const char * restrict src :
+                               itype(restrict _Nt_array_ptr<const char>),
+                             size_t obj_size);
+#endif
+
 #if __has_builtin(__builtin___strncat_chk) || defined(__GNUC__)
 // TODO: we have no way to express the bounds requirement on dest,
 // which needs to be count(strlen(dest) + n).
@@ -58,4 +77,6 @@ char *__builtin___strncpy_chk(char * restrict dest : count(n),
 #ifdef _undef__has_builtin
 #undef _undef__has_builtin
 #undef __has_builtin
+#endif
+
 #endif
