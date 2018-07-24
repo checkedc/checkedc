ea0e687a81a21f215b30f5b7f00be1192bbff950
prabhu <t-prr@microsoft.com>
2018-07-23 18:38:39 -0700

    Verify initializers of NT_CHECKED types are null terminated.
    Added tests to check whether the validation errors are thrown at expected places.

--- tests/static_checking/initializers.c
+++ tests/static_checking/initializers.c
@@ -376,7 +376,7 @@ void f11 (void) checked {
 void f12 (void) {
   struct EmployeeNTChecker {
     int age;
-    char name nt_checked[20];
+    char name nt_checked[10];
   };
 
   struct EmployeeManagerNTChecker {
@@ -395,14 +395,15 @@ void f12 (void) {
   char string_literal_initializer nt_checked[] = "abcde";
   char string_literal_initializer_with_braces nt_checked[] = {"abcde"};
   char string_literal_initializer_with_braces_empty nt_checked[] = {};
-
+  char string_literal_initializer_too_long nt_checked[3] = "abcde"; // expected-warning {{initializer-string for char array is too long}} expected-error {{initializer-string for _Nt_checked char array is too long}}
   char string_literal_initializer_by_chars nt_checked[6] = {'a', 'b', 'c', 'd', 'e', '\0'};
   char string_literal_initializer_by_chars2 nt_checked[5] = {'a', '\0'};
-
+  char string_literal_initializer_by_chars_no_null_term nt_checked[6] = {'a', 'b', 'c', 'd', 'e', 'f'}; // expected-error {{null terminator expected in _Nt_checked array initializer}}
   struct EmployeeNTChecker struct_field_nt_check = {32, {"John"}};
   struct EmployeeManagerNTChecker team =   {{32, {"John"}}, {32, {"Matt"}}};
+  struct EmployeeManagerNTChecker team2 =   {{32, {"John"}}, {32, {"Joseph Matthew Nash"}}}; // expected-warning {{initializer-string for char array is too long}} expected-error {{initializer-string for _Nt_checked char array is too long}}
   struct NumberListNTChecker list1 = {{1,2,3,0}};
   ListChecker list2 = {{1,2,3,4,0}};
-
+  ListChecker list3 = {{1,2,3,4}}; // expected-error {{null terminator expected in _Nt_checked array initializer}}
   int integer_array_checker nt_checked[] = {1, 2, 3, 4, 0};
 }
