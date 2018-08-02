d9c6ba6b66d41cfb08573c790feff93dbd9b2de4
Anna Kornfeld Simpson <t-ansimp@microsoft.com>
2018-08-02 15:18:41 -0700

    Change the bounds-safe interface of free to byte_count 0. We no longer need the extra version of free in checkedc_extensions.

--- include/checkedc_extensions.h
+++ include/checkedc_extensions.h
@@ -16,10 +16,4 @@ extern inline int strncmp_array_ptr(const char *src : count(n), const char *s2 :
   _Unchecked { return strncmp(src, s2, n); }
 }
 
-// default free assumes at least one byte of memory, not always true for nt_arrays
-// TODO: Will be able to do nt_array_ptr<void> after polymorphism implemented
-extern inline void free_nt_array_ptr(void *pointer : byte_count(0)) {
-  _Unchecked { free(pointer); }
-}
-
 #endif /* __CHECKED_C_EXTENSIONS_H */
