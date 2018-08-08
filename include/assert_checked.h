b94b2b9a57fab4847186a4ab6c3a98b22aa74021
Anna Kornfeld Simpson <t-ansimp@microsoft.com>
2018-08-07 18:28:32 -0700

    Add Linux version of assert

--- include/assert_checked.h
+++ include/assert_checked.h
@@ -16,11 +16,14 @@
 #pragma CHECKED_SCOPE ON
 
 #if defined(_WIN32) || defined(_WIN64)
-_ACRTIMP void __cdecl _wassert(_In_z_ wchar_t const *_Message : itype(_Nt_array_ptr>),
-                               _In_z_ wchar_t const *_File : itype(<_Nt_array_ptr>),
+_ACRTIMP void __cdecl _wassert(_In_z_ wchar_t const *_Message : itype(_Nt_array_ptr<const wchar_t>),
+                               _In_z_ wchar_t const *_File : itype(_Nt_array_ptr<const wchar_t>),
                                _In_ unsigned _Line);
 #else
-// not sure what to do on Linux
+extern void __assert(const char *msg : itype(_Nt_array_ptr<const char>), 
+					 const char *file : itype(_Nt_array_ptr<const char>), 
+					 int line);
+
 #endif
 
 #pragma CHECKED_SCOPE OFF
