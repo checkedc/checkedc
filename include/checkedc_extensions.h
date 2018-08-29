08e005ffd11c8926b2ef2764dd82397ed1d46114
Anna Kornfeld Simpson <t-ansimp@microsoft.com>
2018-08-28 17:39:58 -0700

    Fixing a typo in the snprintf_array_ptr definition. Typo was not caught before because my checkedc tests build got corrupted and I had to rebuild clang and the checkedc tests from scratch. The tests caught the typo.

--- include/checkedc_extensions.h
+++ include/checkedc_extensions.h
@@ -19,9 +19,9 @@ extern inline int strncmp_array_ptr(const char *src : count(n), const char *s2 :
 // default snprintf assumes nt_array_ptr for bounds-safe interface
 // this option is for array_ptr
 _Unchecked
-int snprintf_array_ptr(char * restrict s : _Array_ptr<char>) restrict count(n),
+int snprintf_array_ptr(char * restrict s : itype(restrict _Array_ptr<char>) count(n),
                        size_t n, 
-                       const char * restrict format : itype(restrict _Nt_array_ptr<const char>), 
+                       const char * restrict format : itype(restrict _Nt_array_ptr<const char>),
                        ...);
 
 #endif /* __CHECKED_C_EXTENSIONS_H */
