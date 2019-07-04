0ec2c472536477d6ad50599776a570b769a6197c
Abel Nieto <t-abniet@microsoft.com>
2019-07-04 01:24:50 +0100

    Add tests for nested applications

--- tests/typechecking/generic_structs.c
+++ tests/typechecking/generic_structs.c
@@ -45,3 +45,42 @@ struct T4_BoxRef _For_any(T) {
   struct T4_Box<T> BoxRef;
   struct T4_Box<int> IntBox;
 };
+
+//
+// Test that type application applies transitively through
+// multiple structs.
+//
+struct T5_Box1 _For_any(T) {
+  T *x1;
+};
+
+struct T5_Box2 _For_any(T) {
+  struct T5_Box1<T> x2;
+};
+
+struct T5_Box3 _For_any(T) {
+  struct T5_Box2<T> x3;
+};
+
+void T5_TypeApplicationNested() {
+  struct T5_Box3<int> box3;
+  int *x = box3.x3.x2.x1;
+  char *y = box3.x3.x2.x1; // expected-warning {{incompatible pointer types initializing 'char *' with an expression of type 'int *'}}
+}
+
+//
+// Test a struct with multiple type arguments.
+//
+struct T6_Pair _For_any(U, V) {
+  U *u;
+  V *v;
+};
+
+void T6_MultArgs() {
+  struct T6_Pair<int, char> pair1;
+  struct T6_Pair<char, int> pair2;
+  int *p11 = pair1.u;
+  char *p12 = pair1.v;
+  int *p21 = pair2.u;
+  int *p22 = pair2.v;
+}
