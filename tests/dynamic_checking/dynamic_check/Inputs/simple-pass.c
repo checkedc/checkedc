a13e7b860e82765e8e2aa634a11fb50b6f76eb0c
Mandeep Singh Grang <magrang@microsoft.com>
2019-07-22 10:54:22 -0700

    Support target and platform specific testing using sed

--- tests/dynamic_checking/dynamic_check/Inputs/simple-pass.c
+++ tests/dynamic_checking/dynamic_check/Inputs/simple-pass.c
@@ -1,11 +1,14 @@
 // Feature tests of dynamic checking in Checked C.
 //
 // expected-no-diagnostics
+// CHECK: hello
 
+#include <stdio.h>
 #include <stdbool.h>
 #include <stdchecked.h>
 
 int main(void) {
+  printf("hello");
   dynamic_check(true);
 
   return 0;
