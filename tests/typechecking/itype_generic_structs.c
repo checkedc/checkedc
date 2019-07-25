33818182952b18fa9801e5b2b6ba597055d00ee9
Abel Nieto <t-abniet@microsoft.com>
2019-07-25 21:23:53 +0100

    Add simple test for itype-forany

--- tests/typechecking/itype_generic_structs.c
+++ tests/typechecking/itype_generic_structs.c
@@ -26,3 +26,38 @@ void TestItypeInGeneric() {
     _Ptr<char> c2 = charFoo.x;
   }
 }
+
+//
+// Test that we can parse an _Itype_for_any clause in a definition.
+//
+void TestItypeForAnyInDef() {
+  struct Foo _Itype_for_any(T, U) {
+  };
+}
+
+//
+// Test generic bounds safe interface in checked and unchecked scopes.
+//
+void TestItypeForAnyScopes() {
+  struct Foo _Itype_for_any(T) {
+    void *x : itype(_Ptr<T>);	
+  };
+
+  // In an unchecked scope, if no type args are specified, we use void args.
+  struct Foo foo1;
+  void *x1 = foo1.x; // ok: uses declared type (void*) for field 
+  _Ptr<void> y1 = foo1.x; // ok: uses bounds-safe interface (_Ptr<void>)
+
+  struct Foo<char> foo2;
+  char *x2 = foo2.x; // ok: conversion from _Ptr<char> to char*
+  _Ptr<void> y2 = foo2.x; // ok: conversion from void* to _Ptr<void>
+
+  _Checked {
+    struct Foo foo3; // expected-error {{expected '<'}}	expected-error {{unknown type name 'foo3'}}
+
+    struct Foo<char> foo4;
+    _Ptr<void> x4 = foo4.x; // ok: widening conversion
+    _Ptr<int> y4 = foo4.x; // expected-error {{initializing '_Ptr<int>' with an expression of incompatible type '_Ptr<char>'}}
+    _Ptr<char> z4 = foo4.x;
+  }
+}
