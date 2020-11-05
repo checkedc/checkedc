59b90c9758fdd9bf81c1163e17c4875a1bad2235
mwhicks1 <mwh@cs.umd.edu>
2020-11-05 09:03:13 -0500

    Update to assert_checked.h to work on my Mac

--- include/assert_checked.h
+++ include/assert_checked.h
@@ -27,9 +27,18 @@ _ACRTIMP void __cdecl _wassert(_In_z_ wchar_t const *_Message : itype(_Nt_array_
                                _In_z_ wchar_t const *_File : itype(_Nt_array_ptr<const wchar_t>),
                                _In_ unsigned _Line);
 #else
+
+#ifdef __APPLE__
+#define __THROW
+extern void __assert_rtn(const char *msg : itype(_Nt_array_ptr<const char>),
+			 const char *file : itype(_Nt_array_ptr<const char>),
+			 int line,
+			 const char *fn : itype(const char _Nt_checked[]));
+#else
 extern void __assert(const char *msg : itype(_Nt_array_ptr<const char>), 
 					 const char *file : itype(_Nt_array_ptr<const char>), 
 					 int line);
+#endif
 
 #undef __assert_fail
 extern void __assert_fail (const char *__assertion : itype(_Nt_array_ptr<const char>), const char *__file : itype(_Nt_array_ptr<const char>),
