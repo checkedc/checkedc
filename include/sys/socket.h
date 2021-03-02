1c92015dfa5131f5561d26f33ed85df56cb04b78
Sulekha Kulkarni <Sulekha.Kulkarni@microsoft.com>
2021-03-02 11:10:42 -0800

    Added comment header.

--- include/sys/socket.h
+++ include/sys/socket.h
@@ -1,3 +1,10 @@
+//---------------------------------------------------------------------//
+// Wrapper header file that excludes Checked-C-specific declarations   //
+// if implicit inclusion of checked header files is disabled.          //
+//                                                                     //
+/////////////////////////////////////////////////////////////////////////
+
+
 #ifdef NO_IMPLICIT_INCLUDE_CHECKED_HDRS
 #include_next <sys/socket.h>
 #else
