ef274b0e807e6007d39b07c77940fcf46e1943f5
Sulekha Kulkarni <Sulekha.Kulkarni@microsoft.com>
2021-03-02 11:32:28 -0800

    Removed redundant blank lines.

--- include/unistd.h
+++ include/unistd.h
@@ -1,9 +1,7 @@
 //---------------------------------------------------------------------//
 // Wrapper header file that excludes Checked-C-specific declarations   //
 // if implicit inclusion of checked header files is disabled.          //
-//                                                                     //
 /////////////////////////////////////////////////////////////////////////
-
 
 #ifdef NO_IMPLICIT_INCLUDE_CHECKED_HDRS
 #include_next <unistd.h>
