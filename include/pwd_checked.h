42f7679baa70e697985584425baf062a48438bce
mwhicks1 <mwh@correctcomputation.com>
2021-04-12 19:59:42 -0400

    pwd

---
+++ include/pwd_checked.h
@@ -0,0 +1,44 @@
+//---------------------------------------------------------------------//
+// Bounds-safe interfaces for functions in pwd.h that                 //
+// take pointer arguments.                                             //
+//                                                                     //
+// These are listed in the same order that they occur in the C11       //
+// specification.                                                      //
+/////////////////////////////////////////////////////////////////////////
+
+#ifdef __checkedc
+#pragma CHECKED_SCOPE push
+#pragma CHECKED_SCOPE off
+#endif
+
+#include_next <pwd.h>
+
+#ifdef __checkedc
+#pragma CHECKED_SCOPE pop
+#endif
+
+#ifdef __checkedc
+#ifndef __PWD_CHECKED_H
+#define __PWD_CHECKED_H
+
+#pragma CHECKED_SCOPE push
+#pragma CHECKED_SCOPE on
+
+struct passwd *getpwnam(const char *user : itype(_Nt_array_ptr<const char>)) : itype(_Ptr<struct passwd>);
+_Unchecked
+int getpwnam_r(const char *user : itype(_Nt_array_ptr<const char>),
+	       struct passwd *pwd : itype(_Ptr<struct passwd>),
+	       char *buf : byte_count(bufsize),
+	       size_t bufsize, struct passwd **result : itype(_Ptr<_Ptr<struct passwd>>));
+
+struct passwd *getpwuid(uid_t uid) : itype(_Ptr<struct passwd>);
+_Unchecked
+int            getpwuid_r(uid_t uid,
+	       struct passwd *pwd : itype(_Ptr<struct passwd>),
+	       char *buf : byte_count(bufsize),
+	       size_t bufsize, struct passwd **result : itype(_Ptr<_Ptr<struct passwd>>));
+
+#pragma CHECKED_SCOPE pop
+
+#endif // guard
+#endif // Checked C
