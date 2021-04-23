dc82bf139bbe25e23da5b9a0237fe2ccaec3fa2d
Matt McCutchen (Correct Computation) <matt@correctcomputation.com>
2021-04-23 17:28:05 -0400

    Add checked declaration of `poll`.

---
+++ include/poll_checked.h
@@ -0,0 +1,29 @@
+//--------------------------------------------------------------------//
+// Bounds-safe interfaces for functions in poll.h                     //
+//                                                                    //
+////////////////////////////////////////////////////////////////////////
+
+#ifdef __checkedc
+#pragma CHECKED_SCOPE push
+#pragma CHECKED_SCOPE off
+#endif
+
+#include_next <poll.h>
+
+#ifdef __checkedc
+#pragma CHECKED_SCOPE pop
+#endif
+
+#ifdef __checkedc
+#ifndef __POLL_CHECKED_H
+#define __POLL_CHECKED_H
+
+#pragma CHECKED_SCOPE push
+#pragma CHECKED_SCOPE on
+
+extern int poll(struct pollfd fds[] : count(nfds), nfds_t nfds, int timeout);
+
+#pragma CHECKED_SCOPE pop
+
+#endif // guards
+#endif // Checked C
