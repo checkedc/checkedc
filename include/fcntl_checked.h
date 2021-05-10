1b22ff96ad2431e7b10262e25a250d13bb658848
John Kastner <john@correctcomputation.com>
2021-05-10 14:19:05 -0400

    Checked prototype for open

---
+++ include/fcntl_checked.h
@@ -0,0 +1,30 @@
+//---------------------------------------------------------------------//
+// Bounds-safe interfaces for some functions in fcntl.h that           //
+// take pointer arguments.                                             //
+//                                                                     //
+/////////////////////////////////////////////////////////////////////////
+
+#ifdef __checkedc
+#pragma CHECKED_SCOPE push
+#pragma CHECKED_SCOPE off
+#endif
+
+#include_next <fcntl.h>
+
+#ifdef __checkedc
+#pragma CHECKED_SCOPE pop
+#endif
+
+#ifdef __checkedc
+#ifndef __FCNTL_CHECKED_H
+#define __FCNTL_CHECKED_H
+
+int open(const char *pathname : itype(_Nt_array_ptr<const char>), int flags, ...);
+
+#pragma CHECKED_SCOPE push
+#pragma CHECKED_SCOPE on
+
+#pragma CHECKED_SCOPE pop
+
+#endif  // guard
+#endif  // Checked C
