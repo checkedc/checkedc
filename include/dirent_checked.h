a4aa7300aa13a9f9449624e8c2d42970449f98ca
John Kastner <john@correctcomputation.com>
2021-10-07 16:26:24 -0400

    Add checked header for dirent

---
+++ include/dirent_checked.h
@@ -0,0 +1,38 @@
+//---------------------------------------------------------------------//
+// Bounds-safe interfaces for functions in dirent.h that                 //
+// take pointer arguments.                                             //
+//                                                                     //
+/////////////////////////////////////////////////////////////////////////
+
+// The Windows environment may not have dirent.h
+#if defined __has_include_next && __has_include_next(<dirent.h>)
+
+#ifdef __checkedc
+#pragma CHECKED_SCOPE push
+#pragma CHECKED_SCOPE off
+#endif
+
+#include_next <dirent.h>
+
+#ifdef __checkedc
+#pragma CHECKED_SCOPE pop
+#endif
+
+#ifdef __checkedc
+#ifndef __POLL_CHECKED_H
+#define __POLL_CHECKED_H
+
+#pragma CHECKED_SCOPE push
+#pragma CHECKED_SCOPE on
+
+DIR *opendir(const char *name : itype(_Nt_array_ptr<const char>)) : itype(_Ptr<DIR>);
+DIR *fdopendir(int fd) : itype(_Ptr<DIR>);
+
+#pragma CHECKED_SCOPE pop
+
+#endif // guard
+#endif // Checked C
+
+#else // doesn't have dirent.h
+#error "cannot include 'dirent_checked.h' because this system does not have the original 'dirent.h'"
+#endif
